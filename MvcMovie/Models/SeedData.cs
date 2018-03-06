25050302cc12be07d47dc8a1b424a4ef5ed19ecf
Shane Paul Drobnick <sdrobnick@internode.on.net>
2018-03-06 13:40:13 +1000

    Finished

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -24,6 +24,7 @@ namespace MvcMovie.Models
                          Title = "When Harry Met Sally",
                          ReleaseDate = DateTime.Parse("1989-1-11"),
                          Genre = "Romantic Comedy",
+                         Rating = "R",
                          Price = 7.99M
                      },
 
@@ -32,6 +33,7 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters ",
                          ReleaseDate = DateTime.Parse("1984-3-13"),
                          Genre = "Comedy",
+                         Rating = "MA15",
                          Price = 8.99M
                      },
 
@@ -40,16 +42,26 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters 2",
                          ReleaseDate = DateTime.Parse("1986-2-23"),
                          Genre = "Comedy",
+                         Rating = "MA17",
                          Price = 9.99M
                      },
 
-                   new Movie
-                   {
-                       Title = "Rio Bravo",
-                       ReleaseDate = DateTime.Parse("1959-4-15"),
-                       Genre = "Western",
-                       Price = 3.99M
-                   }
+                     new Movie
+                     {
+                         Title = "Rio Bravo",
+                         ReleaseDate = DateTime.Parse("1959-4-15"),
+                         Genre = "Western",
+                         Rating = "PG",
+                         Price = 3.99M
+                     },
+                     new Movie
+                     {
+                         Title = "Space Balls",
+                         ReleaseDate = DateTime.Parse("1987-4-15"),
+                         Genre = "Comedy",
+                         Rating = "MA17",
+                         Price = 6.99M
+                     }
                 );
                 context.SaveChanges();
             }
